faf3c47958ecc9aafc05592e1f49eb959727f897
Kirk Okada <kirkokada@gmail.com>
2017-01-09 03:17:40 +0000

    Set up webpack merge config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge')
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-  // Entry accepts a path or an object of entries.
-  // We'll be using the latter form given it's
-  // convenient with more complex configurations.
-  //
-  // Entries have to resolve to files! It relies on Node.js
-  // convention by default so if a directory contains *index.js*,
-  // it will resolve to that.
-  entry: {
-    app: PATHS.app
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo'
-    })
-  ]
-};
+const common = merge(
+  {
+    entry: {
+      app: PATHS.app
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo'
+      })
+    ]
+  }
+);
+
+module.exports = function(env) {
+  return merge(common);
+}
