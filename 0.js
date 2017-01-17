9b806a0f6a93181a386f6ab6731bfa8daaefe4d7
Kirk Okada <kirkokada@gmail.com>
2017-01-16 15:50:15 -1000

    Updates

---
+++ 0.js
@@ -0,0 +1,13 @@
+webpackJsonp([0],{
+
+/***/ 36:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony default export */ __webpack_exports__["default"] = 'Hello from lazy';
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
